9e46003cc5027c0e401ae21f91d5e959a467d3d0
Raf Rucinski <rafal_rucinski@poczta.fm>
2020-12-13 20:24:27 +0000

    Add project files.

---
+++ TicketsBasket.Models/Domain/JobApplicationStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketsBasket.Models.Domain
+{
+    public enum JobApplicationStatus
+    {
+        Pending = 0,
+        Reviewing = 1,
+        Rejected = 2,
+        Accepted = 3
+    }
+
+}
